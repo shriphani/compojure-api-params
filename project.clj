58da8ec08989974054a5e51c5a8638c60588bb8d
Shriphani Palakodety <shriphanip@gmail.com>
2016-04-18 00:51:07 -0700

    initial commit

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject compojure-api-params "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-time "0.11.0"] ; required due to bug in `lein-ring uberwar`
+                 [metosin/compojure-api "1.0.0"]]
+  :ring {:handler compojure-api-params.handler/app}
+  :uberjar-name "server.jar"
+  :profiles {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]]
+                   :plugins [[lein-ring "0.9.7"]]}})
---
+++ src/compojure_api_params/handler.clj
@@ -0,0 +1,30 @@
+
+(ns compojure-api-params.handler
+  (:require [compojure.api.sweet :refer :all]
+            [ring.util.http-response :refer :all]
+            [schema.core :as s]))
+
+(s/defschema Pizza
+  {:name s/Str
+   (s/optional-key :description) s/Str
+   :size (s/enum :L :M :S)
+   :origin {:country (s/enum :FI :PO)
+            :city s/Str}})
+
+(def app
+  (api
+    {:swagger
+     {:ui "/"
+      :spec "/swagger.json"
+      :data {:info {:title "Compojure-api-params"
+                    :description "Compojure Api example"}
+             :tags [{:name "api", :description "some apis"}]}}}
+
+    (context "/api" []
+      :tags ["api"]
+
+      (GET "/plus" []
+        :return {:result Long}
+        :query-params [x.foo :- Long, y :- Long]
+        :summary "adds two numbers together"
+        (ok {:result (+ x.foo y)})))))
